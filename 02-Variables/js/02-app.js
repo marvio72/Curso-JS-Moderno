19d5046cfa4a23a98a6d24564feddefa7af445a5
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-09-04 20:36:49 -0500

    Todo lo que debes saber de las variables con let

---
+++ 02-Variables/js/02-app.js
@@ -0,0 +1,20 @@
+//Declarar variable con let
+let producto = "Tablet";
+
+//Reasignarle un valor a la variable
+producto = "Pantalla 20 pulgadas"
+
+producto = 20;
+producto = true;
+producto = null;
+
+
+console.log(producto);
+
+
+//Crear variable y luego asignarle un valor 
+let precio;
+
+precio = 100;
+
+console.log(precio);
