85617717494cfc5cd58f503812fc70824ee64e10
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-09-04 19:43:34 -0500

    Comentar el Código en JS

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -1,3 +1,9 @@
+/*
+Este es un comentario multilinea
+*/
+
+//Este es un comentario de una sola linea
+
 const nombre = prompt('Cual es tu nombre?');
 
 document.querySelector('.contenido').innerHTML = `${nombre} está aprendiendo JavaScript Moderno`;
