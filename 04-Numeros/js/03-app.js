994a5076a1ee8eac6363e045af37825841e95caa
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-09-07 17:31:15 -0500

    El Objeto Math

---
+++ 04-Numeros/js/03-app.js
@@ -0,0 +1,40 @@
+let resultado;
+
+// PI
+resultado = Math.PI;
+
+// Redondear
+resultado = Math.round(2.8);
+resultado = Math.round(2.2);
+resultado = Math.round(2.6);
+resultado = Math.round(2.5);
+resultado = Math.round(2.4);
+
+// Redondear hacia arriba
+resultado = Math.ceil(2.1);
+
+// Redondear hacia abajo
+resultado = Math.floor(2.8);
+
+// Raiz Cuadrada
+resultado = Math.sqrt(144);
+
+// Valor Absoluto
+resultado = Math.abs(-400);
+
+// Potencia
+resultado = Math.pow(8,3);
+
+// Minimo
+resultado = Math.min(5,2,3,1,-3);
+
+// Máximo
+resultado = Math.max(5,2,3,1,-3);
+
+// Aleatorio
+resultado = Math.random();
+
+// Aleatorio dentro de un rango... 
+resultado = Math.floor(Math.random() * 30);
+
+console.log(resultado);
