896766fc0ee9ffdee052f3c5f837ff633063fecf
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-09-07 17:10:08 -0500

    Crear numeros en JavaScript

---
+++ 04-Numeros/js/01-app.js
@@ -0,0 +1,19 @@
+// Creación de números
+// Todos los números se crean de la misma forma
+ 
+const numero1 = 30;
+const numero2 = 20;
+const numero3 = 20.20;
+const numero4 = .102030;
+const numero5 = -20;
+
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
+console.log(numero4);
+console.log(numero5);
+
+// No es usual pero es posible
+
+const numero6 = new Number(80);
+console.log(numero6);
