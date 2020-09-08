6bd08e4644c1f33a48479c80bec4cfea36b15a82
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-09-08 18:06:23 -0500

    Buenas practicas a la hora de evaluar un Boolean

---
+++ 06-Booleans/js/03-app.js
@@ -0,0 +1,11 @@
+const autenticado = false;
+
+if(autenticado) {
+    console.log('Si puedes ver netflix');
+} else {
+    console.log('No pueder ver netflix');
+}
+
+// Operador ternario
+
+console.log(autenticado ? 'Si puedes ver netflix' : 'No puedes ver netflix') ;
