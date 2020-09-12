456b8ca1dbc43d48e1fba128d2428b68058e3553
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-09-11 22:40:58 -0500

    Object .keys, .values y .enties

---
+++ 07-Objetos/js/13-app.js
@@ -0,0 +1,14 @@
+const producto = {
+    nombre: "Monitor 20 Pulgadas",
+    precio: 300,
+    disponible: true
+};
+
+// Las llaves de los objetos
+console.log(Object.keys(producto));
+
+// Value te regresa los valores
+console.log(Object.values(producto));
+
+// Entries te regresa todo, keys y values dentro de un arreglo
+console.log(Object.entries(producto));
