9a2e46849c3076f7a91531f5dc5724f11b0b49e3
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-09-14 15:37:15 -0500

    Recorrer un Array

---
+++ 08-Arrays/js/03-app.js
@@ -0,0 +1,13 @@
+const meses = ['Enero','Febrero','Marzo','Abril','Mayo','Junio'];
+
+console.log(meses);
+
+
+ // Cuanto mide el arreglo
+ 
+ console.log(meses.length);
+
+
+ for (let index = 0; index < meses.length; index++) {
+    console.log(meses[index]);
+ }
