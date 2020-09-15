3a5083ff4f2af90a07f412c464f5cb7fec12be92
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-09-15 18:37:14 -0500

    Crear Funciones en JS - Function expression y function Declaration

---
+++ 09-Funciones/js/01-app.js
@@ -0,0 +1,14 @@
+// Declaración de Función (Function Declaration)
+
+function sumar() {
+    console.log(2 + 2);
+}
+
+sumar();
+
+// Expresión de Función (Function Expression)
+const sumar2 = function(){
+    console.log(3 + 3);
+};
+
+sumar2();
