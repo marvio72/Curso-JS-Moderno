4dc577a9f1af1b77703fd537b21ae683b36bb675
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-09-15 19:23:30 -0500

    Parametros y Argumentos en Funciones

---
+++ 09-Funciones/js/05-app.js
@@ -0,0 +1,14 @@
+function sumar(a,b){  //a y b son parametros
+    console.log(a+b);
+}
+
+sumar(2,3); // 2 y 3 son argumentos
+
+sumar(200,187);
+sumar(23,35);
+
+function saludar(nombre, apellido){
+    console.log(`Hola ${nombre} ${apellido}`);
+}
+
+saludar('Marco','Ruvalcaba');
