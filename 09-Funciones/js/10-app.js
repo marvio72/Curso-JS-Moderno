ecfe953b51c24fca4f62bfd7f65f48287c43183d
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-09-16 13:32:51 -0500

    Arrow Functions

---
+++ 09-Funciones/js/10-app.js
@@ -0,0 +1,14 @@
+const aprendiendo = function () {
+    console.log('Aprendiendo JavaScript');
+};
+
+
+// cuando el codigo esta en la misma linea se suprime {}
+const aprendiendo2 = () => console.log('Aprendiendo JavaScript');
+aprendiendo2();
+
+// Mas simplificado aún, esto da como resultado un return
+const aprendiendo3 = () => 'Aprendiendo JavaScript 3';
+console.log(aprendiendo3());
+
+
