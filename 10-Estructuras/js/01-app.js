0ed298d24292018b109b60545d86982b2d239c20
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-09-17 10:34:15 -0500

    Creando un If

---
+++ 10-Estructuras/js/01-app.js
@@ -0,0 +1,10 @@
+const puntaje = 1000;
+
+if (puntaje == 100) {
+    console.log('si es igual...');
+} else {
+    console.log('no es igual...');
+}
+
+// Es lo mismo que lo anterior.
+puntaje == 10 ? console.log('si es igual') : console.log('no es igual');
