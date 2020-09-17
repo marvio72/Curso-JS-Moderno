f47b859d6081f86ec48b2fb985471f6d3f22a66e
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-09-17 10:41:54 -0500

    Comparador Estricto

---
+++ 10-Estructuras/js/02-app.js
@@ -0,0 +1,19 @@
+const puntaje = 1000;
+
+// if (puntaje != 1000) { // != diferente  == igual a
+//     console.log('si es diferente...');
+// } 
+
+// if (puntaje == "1000") { // != diferente  == igual a
+//     console.log('si es igual...');
+// } 
+
+// Forma estricta ===
+if (puntaje === "1000") { // != diferente  == igual a
+    console.log('si es igual...');
+} else {
+    console.log('no es igual...');
+}
+
+// operador no estricto == != valor
+// operador estricto ===  !=== tipo de dato y valor
