ec4fc5df39d0dca0a616355d2483629805d481bb
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-09-17 13:38:36 -0500

    El operador Ternario

---
+++ 10-Estructuras/js/09-app.js
@@ -0,0 +1,5 @@
+const autenticado = false;
+const puedePagar = false;
+
+console.log(autenticado ? puedePagar ? 'Si esta utenticado y puede pagar' : 'Si autenticado, no puede pagar' : 'No esta autenticado');
+
