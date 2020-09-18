6d6804c84bb66730bb1ec3a13ab08fb6449228cf
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-09-17 23:37:01 -0500

    While Loop

---
+++ 11-Iteradores/js/04-app.js
@@ -0,0 +1,8 @@
+// While se repite mientras que la opcion se verdadera
+
+let i = 0;  // Inicializar el While
+
+while (i < 10) { //condición
+    console.log(i % 2 === 0 ? `${i} es par` : `${i} es impar`);
+    i++; // Incremento
+}
