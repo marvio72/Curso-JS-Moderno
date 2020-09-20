cd5c848396b27229bf1f9a2b02dc511ba6b68e77
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-09-19 19:51:45 -0500

    getElementById

---
+++ 13-DOM/js/03-scripts.js
@@ -0,0 +1,9 @@
+const formulario = document.getElementById('formulario');
+console.log(formulario);
+
+const noExiste = document.getElementById('no-existe');
+console.log(noExiste);
+
+// getElementById solo retorna un resultado y es el primero que encuentre.
+
+// estos selectores ya casi no se usan, se usa querySelector
