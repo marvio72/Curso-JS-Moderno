3922a4b956ee865fd7f5a90ec4b0fd5ebdd562ce
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-09-24 12:37:13 -0500

    Leer los datos del curso que seleccionamos

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -15,8 +15,28 @@ function cargarEventListeners() {
 // Funciones
 function agregarCurso(e){
     e.preventDefault();
+
     if (e.target.classList.contains('agregar-carrito')) {
-        console.log('Agregando al carrito...');
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosCurso(cursoSeleccionado);
 
     } 
+}
+
+/*
+Lee el contenido del HTML al que le dimos click y extrae la información del curso
+*/
+function leerDatosCurso(curso){
+    // console.log(curso);
+    
+    // Crear un objeto con el contenido del curso actual
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    };
+    
+    console.log(infoCurso);
 }
