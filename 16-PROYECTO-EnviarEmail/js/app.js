7ae05145008a6c48417c856f6e0e7339c757815f
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-09-25 17:51:41 -0500

    Validar los Campos

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -2,11 +2,25 @@
 Variables
 */
 
+// Botones
 const btnEnviar = document.querySelector('#enviar');
+
+// Campos
+const email   = document.querySelector('#email');
+const asunto  = document.querySelector('#asunto');
+const mensaje = document.querySelector('#mensaje');
+
 
 eventListeners();
 function eventListeners() {
+    // Cuando la app arranca
     document.addEventListener('DOMContentLoaded', iniciarApp);
+
+    // Campos del formulario
+    email.addEventListener('blur', validarFormulario);
+    asunto.addEventListener('blur', validarFormulario);
+    mensaje.addEventListener('blur', validarFormulario);
+
 }
 
 
@@ -19,7 +33,19 @@ function eventListeners() {
 /*
 Funciones
 */
+// Iniciando la app
 function iniciarApp(){
     btnEnviar.disabled = true;
     btnEnviar.classList.add('cursor-not-allowed', 'opacity-50');
+}
+
+// Validando el formulario
+function validarFormulario(e){
+    if (e.target.value.length > 0) {
+        console.log(e.target.value);
+    } else {
+        // e.target.style.borderBottomColor =  'red';
+        e.target.classList.add('border', 'border-red-500');
+    }
+   
 }
