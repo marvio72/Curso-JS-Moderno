1a560d9680aa8cf558b173cb06a6fee3e9ae73df
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-10-01 13:57:36 -0500

    Leer el valor de la marca del automovil

--- 17-PROYECTO-Buscador/js/app.js
+++ 17-PROYECTO-Buscador/js/app.js
@@ -1,11 +1,34 @@
 /*
 Variables
 */
-const resultado = document.querySelector('#resultado');
-const year = document.querySelector('#year');
 
+
+const marca = document.querySelector('#marca');
+const year = document.querySelector('#year');
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
+const color = document.querySelector('#color');
+
+// Contenedor para los resultados
+const resultado = document.querySelector('#resultado');
+
+// Años maximos y minimos
 const max = new Date().getFullYear();
 const min = max - 10;
+
+// Generar un objeto con la búsqueda
+const datosBusqueda = {
+    marca: '',
+    year: '',
+    minimo: '',
+    maximo: '',
+    puertas: '',
+    transmision: '',
+    color: ''
+};
+
 /*
 Eventos
 */
@@ -15,6 +38,36 @@ document.addEventListener('DOMContentLoaded', () => {
     // Lena las opciones de años
     llenarSelect();
 });
+
+// Event listener para los select de búsqueda
+marca.addEventListener('change', e => {
+    datosBusqueda.marca = e.target.value;
+});
+
+year.addEventListener('change', e => {
+    datosBusqueda.year = e.target.value;
+});
+
+minimo.addEventListener('change', e => {
+    datosBusqueda.minimo = e.target.value;
+});
+
+maximo.addEventListener('change', e => {
+    datosBusqueda.maximo = e.target.value;
+});
+
+puertas.addEventListener('change', e => {
+    datosBusqueda.puertas = e.target.value;
+});
+
+transmision.addEventListener('change', e => {
+    datosBusqueda.transmision = e.target.value;
+});
+
+color.addEventListener('change', e => {
+    datosBusqueda.color = e.target.value;
+});
+
 
 /*
 Funciones
