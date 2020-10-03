4af7286faf6f99882ee1da2b31a6e9071efd6d44
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-10-02 20:23:06 -0500

    Primeros pasos Proyecto localStage

---
+++ 19-PROYECTO-LocalStorage/js/app.js
@@ -0,0 +1,25 @@
+// Variables
+const formulario = document.querySelector('#formulario');
+const listaTweets = document.querySelector('.lista-tweets');
+let tweets = [];
+
+
+
+
+// Event Listeners
+eventListeners();
+
+function eventListeners(){
+    formulario.addEventListener('submit', agregarTweet);
+}
+
+
+
+
+// Funciones
+function agregarTweet(e) {
+    e.preventDefault();
+
+    
+    console.log('Agregando Tweet');
+}
