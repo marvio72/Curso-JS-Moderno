89e2a0801822f38e4a5a5c3d3956b7ea865ac7bd
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-10-02 20:44:40 -0500

    Validando el Formulario

--- 19-PROYECTO-LocalStorage/js/app.js
+++ 19-PROYECTO-LocalStorage/js/app.js
@@ -21,5 +21,33 @@ function agregarTweet(e) {
     e.preventDefault();
 
     
-    console.log('Agregando Tweet');
+    // Textarea donde el usuario escribe
+    const tweet = document.querySelector('#tweet').value;
+   
+
+    // Validación de campo vacio
+    if (tweet === '') {
+        mostraError('Un mensaje no puede ir vacio');
+
+        return; // evita que se ejecuten más lineas de código
+    } 
+    
+    console.log('agregando tweet');
+}
+
+// Mostrar mensaje de erro
+function mostraError(error){
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = error;
+    mensajeError.classList.add('error');
+
+    // Insertarlo en el contenito
+    const contenido = document.querySelector('#contenido');
+    contenido.appendChild(mensajeError);
+
+    // Elimina la alerta despues de 3 segundos
+    setTimeout(() => {
+        mensajeError.remove();
+    }, 3000);
+    
 }
