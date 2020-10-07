0ca724cd5f9d2fb27293fd61f8f859acbe736319
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-10-06 23:30:31 -0500

    El objeto Date

---
+++ 21-Fechas/js/01-app.js
@@ -0,0 +1,21 @@
+const diaHoy = new Date();
+
+
+let valor;
+
+valor = diaHoy;
+
+valor = diaHoy.getFullYear();
+valor = diaHoy.getMonth(); // inicia con 0 como enero
+valor = diaHoy.getDay();
+valor = diaHoy.getHours();
+valor = diaHoy.getMinutes();
+valor = diaHoy.getTime();
+// valor = diaHoy.setFullYear(2010);
+
+
+
+
+
+
+console.log(valor)
