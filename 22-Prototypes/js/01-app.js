bd6ef0df04ab34c3fc8c09ae431644a07c8901fa
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-10-08 15:09:07 -0500

    Que es el proto y crear un tipo de objeto nuevo

---
+++ 22-Prototypes/js/01-app.js
@@ -0,0 +1,19 @@
+// Objetos literales, no son versatiles
+const cliente = {
+    nombre: "Marco",
+    saldo: 500
+};
+
+console.log(cliente);
+
+// Programación orientada a Objetos.
+// Creamos un constructor.
+// Objetos dinamicos y reutilizables.
+
+function Cliente(nombre, saldo) {
+    this.nombre = nombre;
+    this.saldo = saldo;
+};
+
+const marco = new Cliente('Marco', 500);
+console.log(marco);
