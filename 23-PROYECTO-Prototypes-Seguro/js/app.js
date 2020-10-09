c4e2a90aaa99cde56b5ef86c47de164b81f79174
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-10-09 13:48:04 -0500

    Validar el Formulario

--- 23-PROYECTO-Prototypes-Seguro/js/app.js
+++ 23-PROYECTO-Prototypes-Seguro/js/app.js
@@ -24,8 +24,36 @@ UI.prototype.llenarOpciones = () => {
 
 // Instanciar unicodeBidi: 
 const ui = new UI();
-console.log(ui);
+
 
 document.addEventListener('DOMContentLoaded', () => {
     ui.llenarOpciones(); // Llena el select con los años
 });
+
+eventListeners();
+
+function eventListeners() {
+    const formulario = document.querySelector("#cotizar-seguro");
+    formulario.addEventListener('submit', cotizarSeguro);
+}
+
+// Al ser un evento agregamos e
+function cotizarSeguro(e){
+    e.preventDefault();
+
+    // Leer la marca seleccionada
+    const marca = document.querySelector('#marca').value;
+    
+    // leer el año seleccionado
+    const year = document.querySelector('#year').value;
+
+    // leer el tipo de cobertura
+    const tipo = document.querySelector('input[name="tipo"]:checked').value;
+    
+    if (marca === '' || year === '' || tipo === '') {
+        console.log('No paso la validación');
+    } else {
+        console.log('Si paso la validación');
+    }
+    
+}
