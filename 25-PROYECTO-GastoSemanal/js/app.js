6942af7a12e50b831036d553715bed3b184dada1
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-10-10 20:47:58 -0500

    Creando e Instanciando la Clase de presupuesto

--- 25-PROYECTO-GastoSemanal/js/app.js
+++ 25-PROYECTO-GastoSemanal/js/app.js
@@ -11,15 +11,34 @@ function eventListeners() {
 
 
 // Clases
+class Presupuesto {
+    constructor(presupuesto){
+        this.presupuesto = Number(presupuesto);
+        this.restante = Number(presupuesto);
+        this.gastos = [];
+    }
+}
+
+class UI {
+
+}
+
+//Instanciar
+const ui = new UI();
+let presupuesto
 
 
 // Funciones
 function preguntarPresupuesto() {
     const presupuestoUsuario = prompt('¿Cual es tu presupuesto?');
 
-    console.log(Number(presupuestoUsuario));
+    // console.log(Number(presupuestoUsuario));
 
     if (presupuestoUsuario === '' || presupuestoUsuario <= 0 || isNaN(presupuestoUsuario  || presupuestoUsuario === null)) {
         window.location.reload();
     }
+
+    // Presupuesto valido
+    presupuesto = new Presupuesto(presupuestoUsuario);
+    console.log(presupuesto);
 }
