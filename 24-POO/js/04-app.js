0eff556341a33c4cfa12cc360df1f3ca9f377d7f
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-10-10 19:36:41 -0500

    Propiedades Privadas en JavaScript

---
+++ 24-POO/js/04-app.js
@@ -0,0 +1,18 @@
+class Cliente {
+
+    // Parametro privado
+
+    #nombre;
+   
+    setNombre(nombre) {
+        this.#nombre = nombre;
+    }
+
+    getNombre(){
+        return this.#nombre;
+    }
+}
+
+const juan = new Cliente();
+juan.setNombre('Juan');
+console.log(juan.getNombre());
