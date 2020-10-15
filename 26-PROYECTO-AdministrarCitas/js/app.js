7dccf4332c2ca7a9d06b2069f03ef8a693e23e01
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-10-14 20:11:40 -0500

    Primeros pasos proyecto citas

---
+++ 26-PROYECTO-AdministrarCitas/js/app.js
@@ -0,0 +1,38 @@
+// Variable de Campos
+const mascotaInput = document.querySelector('#mascota');
+const propietarioInput = document.querySelector('#propietario');
+const telefonoInput = document.querySelector('#telefono');
+const fechaInput = document.querySelector('#fecha');
+const horaInput = document.querySelector('#hora');
+const sintomasInput = document.querySelector('#sintomas');
+
+// UI
+const formulario = document.querySelector('#nueva-cita');
+const contenedorCitas = document.querySelector('#citas');
+
+// Registra Eventos
+eventListeners();
+function eventListeners(){
+    mascotaInput.addEventListener('input', datosCita);
+    propietarioInput.addEventListener('input', datosCita);
+    telefonoInput.addEventListener('input', datosCita);
+    fechaInput.addEventListener('input', datosCita);
+    horaInput.addEventListener('input', datosCita);
+    sintomasInput.addEventListener('input', datosCita);
+}
+
+// Objeto con la información de la cita
+const citaObj = {
+    mascota: '',
+    propietario: '',
+    telefono: '',
+    fecha: '',
+    hora: '',
+    sintomas: ''
+}
+
+
+// Agrega datos al objeto de cita
+function datosCita(e){
+    citaObj[e.target.name] = e.target.value;    
+}
