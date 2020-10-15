d13d966710aaecbbe9128444080e24c336e7c928
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-10-14 20:46:08 -0500

    Validación y Mostrar un mensaje de error

--- 26-PROYECTO-AdministrarCitas/js/app.js
+++ 26-PROYECTO-AdministrarCitas/js/app.js
@@ -5,10 +5,51 @@ const telefonoInput = document.querySelector('#telefono');
 const fechaInput = document.querySelector('#fecha');
 const horaInput = document.querySelector('#hora');
 const sintomasInput = document.querySelector('#sintomas');
+// Constante de tiempo en settimeout 
+const tiempo = 1000;
 
 // UI
 const formulario = document.querySelector('#nueva-cita');
 const contenedorCitas = document.querySelector('#citas');
+
+
+// Class
+class Citas {
+    constructor(){
+        this.citas = [];
+    }
+}
+
+class UI {
+    imprimirAlerta(mensaje, tipo){
+        // Crear el div
+        const divMensaje = document.createElement('div');
+        divMensaje.className = ('text-center alert d-block col-12');
+
+        // Agregar clase en base al tipo de console.error
+        if (tipo === 'error') {
+            divMensaje.classList.add('alert-danger');
+        } else {
+            divMensaje.classList.add('alert-success');
+        }
+
+        // Mensaje de error
+        divMensaje.textContent = mensaje;
+
+        // Mostrarlo en el HTML
+        document.querySelector('#contenido').insertBefore(divMensaje, document.querySelector('.agregar-cita'));
+
+        // Borrar el Mensaje en un tiempo
+        setTimeout(() => {
+            divMensaje.remove();
+        }, tiempo);
+
+    }
+}
+
+// Instanciar las clases
+const ui = new UI();
+const administrarCitas = new Citas();
 
 // Registra Eventos
 eventListeners();
@@ -19,6 +60,8 @@ function eventListeners(){
     fechaInput.addEventListener('input', datosCita);
     horaInput.addEventListener('input', datosCita);
     sintomasInput.addEventListener('input', datosCita);
+
+    formulario.addEventListener('submit', nuevaCita);
 }
 
 // Objeto con la información de la cita
@@ -35,4 +78,22 @@ const citaObj = {
 // Agrega datos al objeto de cita
 function datosCita(e){
     citaObj[e.target.name] = e.target.value;    
+}
+
+// Valida y agrega una nueva cita a la clase de citas
+function nuevaCita(e){
+    e.preventDefault();
+    // Extraer la informacvión del objeto de cita 
+    const { mascota, propietario, telefono, fecha, hora, sintomas } = citaObj;
+
+    // validar
+    if (mascota === '' || propietario === '' || telefono === '' || fecha === '' || hora === '' || sintomas === '') {
+        ui.imprimirAlerta('Todos los campos son obligatorios', 'error');
+
+        return;
+    }
+
+    // creando una nueva cita
+    
+
 }
