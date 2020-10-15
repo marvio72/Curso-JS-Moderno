00d6b36646fff0f44f0686bd62d12b099c974df9
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-10-14 22:50:40 -0500

    Eliminar las citas

--- 26-PROYECTO-AdministrarCitas/js/app.js
+++ 26-PROYECTO-AdministrarCitas/js/app.js
@@ -23,6 +23,10 @@ class Citas {
         this.citas = [...this.citas, cita];
         // console.log(this.citas);
     }
+
+    eliminarCita(id){
+        this.citas = this.citas.filter(cita => cita.id != id)
+    }
 }
 
 class UI {
@@ -90,6 +94,13 @@ class UI {
                 <span class="font-weight-bolder">Síntomas: </span> ${sintomas}
             `;
 
+            // Boton para eliminar
+            const btnEliminar = document.createElement('button');
+            btnEliminar.classList.add('btn', 'btn-danger', 'mr-2');
+            btnEliminar.innerHTML = 'Eliminar <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>';
+
+            btnEliminar.onclick = () => eliminarCita(id);
+
 
             // Agregar los parrafos a divCita
             divCita.appendChild(mascotaParrafo);
@@ -98,6 +109,7 @@ class UI {
             divCita.appendChild(fechaParrafo);
             divCita.appendChild(horaParrafo);
             divCita.appendChild(sintomasParrafo);
+            divCita.appendChild(btnEliminar);
     
 
             // Agregar las citas al HTML
@@ -182,5 +194,14 @@ function reiniciarObjeto(){
     citaObj.fecha = '';
     citaObj.hora = '';
     citaObj.sintomas = '';
+}
+
+function eliminarCita(id) {
+    // Eliminar la cita
+    administrarCitas.eliminarCita(id);
+    // Muestre un mensaje
+    ui.imprimirAlerta('El registro se borro con exito');
+    // Refrescar las citas
+    ui.imprimirCitas(administrarCitas);
 }
 
