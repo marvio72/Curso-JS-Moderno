1e9b7ccc1f317d2e84b39b5132a63864592bd272
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-10-21 18:58:10 -0500

    Creando los diferentes archivos proyecto citas

---
+++ 29-PROYECTO-CitasModulos/js/app.js
@@ -0,0 +1,17 @@
+
+
+formulario.addEventListener('submit', nuevaCita);
+
+
+
+// Eventos
+eventListeners();
+function eventListeners() {
+    mascotaInput.addEventListener('change', datosCita);
+    propietarioInput.addEventListener('change', datosCita);
+    telefonoInput.addEventListener('change', datosCita);
+    fechaInput.addEventListener('change', datosCita);
+    horaInput.addEventListener('change', datosCita);
+    sintomasInput.addEventListener('change', datosCita);
+}
+
