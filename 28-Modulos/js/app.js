052fd3b27fa74caf6d223b776c07cfe8f1b1adca
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-10-21 17:14:11 -0500

    Exportar e Importar Funciones

--- 28-Modulos/js/app.js
+++ 28-Modulos/js/app.js
@@ -1,4 +1,8 @@
-import { ahorro, nombreCliente } from './cliente.js';
+import { ahorro, mostrarInformacion, nombreCliente, tieneSaldo} from './cliente.js';
 
 console.log(nombreCliente);
 console.log(ahorro);
+
+console.log(mostrarInformacion(nombreCliente, ahorro));
+
+tieneSaldo(ahorro);
--- 28-Modulos/js/cliente.js
+++ 28-Modulos/js/cliente.js
@@ -1,6 +1,24 @@
-export const nombreCliente = 'Marco';
-export const ahorro = 200;
+const nombreCliente = 'Marco';
+const ahorro = 200;
 
 
 
+function mostrarInformacion(nombre, ahorro) {
+    return `Cliente: ${nombre} Ahorro: ${ahorro}`;
+}
 
+function tieneSaldo(ahorro) {
+    if (ahorro > 0) {
+        console.log('Si tiene Saldo');
+    } else {
+        console.log('Ya no tiene saldo');
+    }
+}
+
+
+export {
+    nombreCliente,
+    ahorro,
+    mostrarInformacion,
+    tieneSaldo
+}
