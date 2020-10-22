51e631ce072ffea77fa5c48a74b7891fd5d49cf7
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-10-22 10:51:14 -0500

    Creando la base de datos Proyecto 30

---
+++ 30-IndexedDB/js/app.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    crmDB();
+});
+
+function crmDB() {
+    // Crear base de datos version 1.0
+    let crmDB = window.indexedDB.open('crm', 1);
+
+    // Si hay un error
+    crmDB.onerror = function() {
+        console.log('Hubo un error a la hora de crear la BD');
+    }
+
+    // Si se creo bien
+    crmDB.onsuccess = function() {
+        console.log('Base de datos creada');
+    }
+
+    // Configuración de la base de datos
+    crmDB.onupgradeneeded = function () {
+        console.log('Solo se ejecuta una vez');
+    }
+}
