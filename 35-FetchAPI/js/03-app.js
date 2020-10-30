d81d53c372615408e42beae4de757afc7d4503d4
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-10-30 11:52:13 -0600

    Consultar e Imprimir los Resultados de un Fetch

---
+++ 35-FetchAPI/js/03-app.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', obtenerDatos() );
+
+const cargarJSONArrayBtn = document.querySelector('#cargarJSONArray');
+
+cargarJSONArrayBtn.addEventListener('click', obtenerDatos);
+
+function obtenerDatos() {
+    const url = 'data/empleados.json';
+
+    fetch(url)
+        .then( respuesta => respuesta.json() )
+        .then( datos => mostrarHTML(datos) )
+}
+
+
+function mostrarHTML(datos) {
+    
+    const contenido = document.querySelector('div.contenido');
+    
+    const datosDiv = document.createElement('div');
+
+    datos.forEach(dato => {
+        const {id, nombre, empresa, trabajo} = dato;
+        datosDiv.innerHTML += `
+            <p>id: ${id}</p>
+            <p>Nombre: ${nombre}</p>
+            <p>Empresa: ${empresa}</p>
+            <p>Trabajo: ${trabajo}</p>
+            <hr>
+        `;
+
+        contenido.appendChild(datosDiv);
+    });
+   
+}
