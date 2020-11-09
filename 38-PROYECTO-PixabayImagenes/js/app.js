9494f2654e7c6376d05bea73fda634648f23d4db
Marco Ruvalcaba Espinoza <marcoruvalcabaespinoza@MacBook-Pro-de-Marco.local>
2020-11-09 14:41:20 -0600

    Mostrar los Resultados de la consulta

--- 38-PROYECTO-PixabayImagenes/js/app.js
+++ 38-PROYECTO-PixabayImagenes/js/app.js
@@ -44,7 +44,7 @@ function mostrarAlerta(mensaje) {
 
 function buscarImagenes(termino){
     const key = '2376860-251f8986c8d4bad53dc7116fb';
-    const url = `https://pixabay.com/api/?key=${key}&q=${termino}`;
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=100`;
 
     fetch(url)
         .then(respuesta => respuesta.json())
@@ -56,4 +56,35 @@ function buscarImagenes(termino){
 
 function mostrarImagenes(imagenes){
     console.log(imagenes);
+
+    while (resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
+
+    // Iterar sobre el arreglo de imagenes y Construir el HTML
+    imagenes.forEach(imagen => {
+        const { previewURL, likes, views, largeImageURL} = imagen;
+
+        resultado.innerHTML += `
+            <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
+                <div class="bg-white">
+                    <img class="w-full" src="${previewURL}">
+                    <div class="p-4">
+                        <p class="font-bold"> ${likes} <span class="font-light"> Me Gusta</span></p>
+                        <p class="font-bold"> ${views} <span class="font-light"> Vistas</span></p>
+                        
+                        <a 
+                            class="block w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center rounded mt-5 p-1"
+                            href="${largeImageURL}" target="_blanck" rel="noopener noreferrer">
+                            Ver Imagen
+                        </a>
+                    </div>
+                </div>
+            </div>
+        `
+
+
+        
+    });
+
 }
