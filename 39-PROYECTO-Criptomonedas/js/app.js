2f4dc8c4b663479a3eb6791ef0306a34dd6d3a66
Marco Ruvalcaba Espinoza <marcoruvalcabaespinoza@MacBook-Pro-de-Marco.local>
2020-11-23 19:12:09 -0600

    Leer la moneda y criptomoneda seleccionada

--- 39-PROYECTO-Criptomonedas/js/app.js
+++ 39-PROYECTO-Criptomonedas/js/app.js
@@ -1,4 +1,11 @@
-const criptomonedasSelect = document.querySelector("#criptomonedas");
+const criptomonedasSelect = document.querySelector('#criptomonedas');
+const monedaSelect = document.querySelector('#moneda');
+const formulario = document.querySelector('#formulario');
+
+const objBusqueda = {
+  moneda: '',
+  criptomoneda: '',
+};
 
 // Crear un Promise
 const obtenerCriptomonedas = (criptomonedas) =>
@@ -6,13 +13,18 @@ const obtenerCriptomonedas = (criptomonedas) =>
     resolve(criptomonedas);
   });
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   consultarCriptomonedas();
+
+  formulario.addEventListener('submit', submitFormulario);
+
+  criptomonedasSelect.addEventListener('change', leerValor);
+  monedaSelect.addEventListener('change', leerValor);
 });
 
 function consultarCriptomonedas() {
   const url =
-    "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD";
+    'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
 
   fetch(url)
     .then((respuesta) => respuesta.json())
@@ -24,9 +36,29 @@ function selectCriptomonedas(criptomonedas) {
   criptomonedas.forEach((cripto) => {
     const { FullName, Name } = cripto.CoinInfo;
 
-    const option = document.createElement("option");
+    const option = document.createElement('option');
     option.value = Name;
     option.textContent = FullName;
     criptomonedasSelect.appendChild(option);
   });
+}
+
+function leerValor(e) {
+  objBusqueda[e.target.name] = e.target.value;
+}
+
+function submitFormulario(e) {
+  e.preventDefault();
+
+  // validar
+  const { moneda, criptomoneda } = objBusqueda;
+
+  if (moneda === '' || criptomoneda === '') {
+    mostrarAlerta('Ambos campos son necesarios');
+    return;
+  }
+}
+
+function mostrarAlerta(msg) {
+  console.log(msg);
 }
