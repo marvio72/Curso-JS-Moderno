33067e7894f22f6af07d7ed04a53a81e7b5d2cea
Marco Ruvalcaba Espinoza <marcoruvalcabaespinoza@MacBook-Pro-de-Marco.local>
2020-11-23 21:53:05 -0600

    Mostrando un Spinner de carga

--- 39-PROYECTO-Criptomonedas/js/app.js
+++ 39-PROYECTO-Criptomonedas/js/app.js
@@ -27,6 +27,8 @@ function consultarCriptomonedas() {
   const url =
     'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
 
+  mostrarSpinner();
+
   fetch(url)
     .then((respuesta) => respuesta.json())
     .then((resultado) => obtenerCriptomonedas(resultado.Data))
@@ -125,4 +127,19 @@ function limpiarHTML() {
   while (resultado.firstChild) {
     resultado.removeChild(resultado.firstChild);
   }
+}
+
+function mostrarSpinner() {
+  limpiarHTML();
+
+  const spinner = document.createElement('div');
+  spinner.classList.add('spinner');
+
+  spinner.innerHTML = `
+        <div class="bounce1"></div>
+        <div class="bounce2"></div>
+        <div class="bounce3"></div>
+  `;
+
+  resultado.appendChild(spinner);
 }
