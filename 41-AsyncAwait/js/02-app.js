f4627aaf1444daab8177fe97ed7ca759ee2a303b
Marco Ruvalcaba Espinoza <marcoruvalcabaespinoza@MacBook-Pro-de-Marco.local>
2020-12-02 18:15:42 -0600

    Nuestro primer ejemplo de Async Await y que es lo que hace

---
+++ 41-AsyncAwait/js/02-app.js
@@ -0,0 +1,27 @@
+function descargarClientes() {
+  return new Promise((resolve, reject) => {
+    const error = false;
+
+    setTimeout(() => {
+      if (!error) {
+        resolve('El listado de Clientes se descargo correctamente');
+      } else {
+        reject('Error en la conexión');
+      }
+    }, 3000);
+  });
+}
+
+// Async await
+async function ejecutar() {
+  try {
+    console.log(1 + 1);
+    const respuesta = await descargarClientes();
+    console.log(2 + 2);
+    console.log(respuesta);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+ejecutar();
