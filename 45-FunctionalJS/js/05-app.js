1f2aaf790c6ba8d73d2b3c8cd55ca1fcc7250ba2
marvio72 <mruvalcaba@oversistemas.com>
2020-12-17 10:59:23 -0600

    Menos cantidad de código en tus Funciones

---
+++ 45-FunctionalJS/js/05-app.js
@@ -0,0 +1,22 @@
+const carrito = [
+  { nombre: 'Monitor 20 Pulgadas', precio: 500 },
+  { nombre: 'Televisión 50 Pulgadas', precio: 700 },
+  { nombre: 'Tablet', precio: 300 },
+  { nombre: 'Audifonos', precio: 200 },
+  { nombre: 'Teclado', precio: 50 },
+  { nombre: 'Celular', precio: 500 },
+  { nombre: 'Bocinas', precio: 300 },
+  { nombre: 'Laptop', precio: 800 },
+];
+
+// const obtenerNombres = (producto) => {
+//   return producto.nombre;
+// };
+
+const obtenerNombres = (p) => p.nombre;
+const resultado = carrito.map(obtenerNombres);
+console.log(resultado);
+
+const mayor400 = (p) => p.precio > 400;
+const resultado1 = carrito.filter(mayor400);
+console.log(resultado1);
