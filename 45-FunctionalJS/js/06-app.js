3885689c8ac91d948e9533c8153dffd568d32f0e
marvio72 <mruvalcaba@oversistemas.com>
2020-12-17 11:14:37 -0600

    Pure Functions

---
+++ 45-FunctionalJS/js/06-app.js
@@ -0,0 +1,13 @@
+// Funciones puras o pure Functions
+
+// const duplicar = (numero) => {
+//     return numero * 2;
+// }
+
+// Lo mismo que el anterior
+const numero1 = 20;
+const duplicar = (numero) => numero * 2;
+
+const resultado = duplicar(numero1);
+console.log(resultado);
+console.log(numero1);
