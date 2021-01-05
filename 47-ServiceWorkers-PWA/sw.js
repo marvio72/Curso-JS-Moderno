67864b02f671f5d822c98fc3cdb19fe1820a9e31
marvio72 <mruvalcaba@oversistemas.com>
2021-01-05 16:47:57 -0600

    Agregar una página de error cuando no hay conexión

--- 47-ServiceWorkers-PWA/sw.js
+++ 47-ServiceWorkers-PWA/sw.js
@@ -2,6 +2,7 @@ const nombreCache = 'apv-v1';
 const archivos = [
   './',
   './index.html',
+  './error.html',
   './css/bootstrap.css',
   './css/styles.css',
   './js/app.js',
@@ -29,11 +30,13 @@ self.addEventListener('activate', (e) => {
 
 // Evento fetch para descargar archivo estatico
 self.addEventListener('fetch', (e) => {
-  console.log('Fetch... ', e);
+  console.log('Fetch', e);
 
   e.respondWith(
-    caches.match(e.request).then((respuestaCache) => {
-      return respuestaCache;
-    })
+    caches
+      .match(e.request)
+      .then((cacheResponse) =>
+        cacheResponse ? cacheResponse : caches.match('error.html')
+      )
   );
 });
