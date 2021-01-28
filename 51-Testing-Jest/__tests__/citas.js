4d23e57f7e17e2dc5fdb29a2d5c52a0fae1a003b
marvio72 <mruvalcaba@oversistemas.com>
2021-01-27 22:19:43 -0600

    Testing al Proyecto de Citas - Actualizar y Eliminar

--- 51-Testing-Jest/__tests__/citas.js
+++ 51-Testing-Jest/__tests__/citas.js
@@ -3,8 +3,11 @@ import Citas from '../js/classes/Citas';
 describe('Probar la clase de citas', () => {
   const citas = new Citas();
 
+  const id = Date.now();
+
   test('Agregar una nueva cita', () => {
     const citaObj = {
+      id,
       mascota: 'Roco',
       propietario: 'Marco',
       telefono: '33434321',
@@ -13,11 +16,31 @@ describe('Probar la clase de citas', () => {
       sintomas: 'Solo duerme',
     };
 
-    citaObj.id = Date.now();
-
     citas.agregarCita(citaObj);
 
     // Prueba
+    expect(citas).toMatchSnapshot();
+  });
+
+  test('Actualizar cita', () => {
+    const citaActualizada = {
+      id,
+      mascota: 'Kiara',
+      propietario: 'Marco',
+      telefono: '33434321',
+      fecha: '10-12-2020',
+      hora: '10:30',
+      sintomas: 'Solo duerme',
+    };
+
+    citas.editarCita(citaActualizada);
+
+    expect(citas).toMatchSnapshot();
+  });
+
+  test('Eliminar una cita', () => {
+    citas.eliminarCita(id);
+
     expect(citas).toMatchSnapshot();
   });
 });
