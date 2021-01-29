d8b2fab0678e3df3d7f22c16116e1f20076d75f3
marvio72 <mruvalcaba@oversistemas.com>
2021-01-29 00:32:15 -0600

    Eliminar Citas

---
+++ 52-Testing-Cypress/cypress/integration/eliminar_cita.spect.js
@@ -0,0 +1,39 @@
+/// <reference types="cypress" />
+
+describe('Llena los campos para una nueva cita y la elimina', () => {
+  it('Campos nueva cita', () => {
+    cy.visit('/index.html');
+
+    // Campos y boton
+    cy.get('[data-cy=mascota-input]').type('Rocco');
+    cy.get('[data-cy=propietario-input]').type('Marco Ruvalcaba');
+    cy.get('[data-cy=telefono-input]').type('32323232');
+    cy.get('[data-cy=fecha-input]').type('2020-12-23');
+    cy.get('[data-cy=hora-input]').type('10:30');
+    cy.get('[data-cy=sintomas-textarea]').type('Surra muy apestoso');
+    cy.get('[data-cy=submit-cita]').click();
+
+    // Encabezado
+    cy.get('[data-cy=citas-heading]').invoke('text').should('equal', 'Administra tus Citas');
+
+    // Alerta
+
+    cy.get('[data-cy=alerta]').invoke('text').should('equal', 'Se agregó correctamente');
+
+    cy.get('[data-cy=alerta]').should('have.class', 'alert-success');
+  });
+
+  it('Eliminar una cita', () => {
+    cy.wait(3000);
+
+    cy.get('[data-cy=btn-eliminar]').click();
+
+    cy.get('[data-cy=citas-heading]').invoke('text').should('equal', 'No hay Citas, comienza creando una');
+
+    // Alertas
+
+    cy.get('[data-cy=alerta]').invoke('text').should('equal', 'Se eliminó Correctamente');
+
+    cy.get('[data-cy=alerta]').should('have.class', 'alert-success');
+  });
+});
--- 52-Testing-Cypress/js/classes/UI.js
+++ 52-Testing-Cypress/js/classes/UI.js
@@ -76,6 +76,8 @@ class UI {
       const btnEliminar = document.createElement('button');
       btnEliminar.onclick = () => eliminarCita(id); // añade la opción de eliminar
 
+      btnEliminar.dataset.cy = 'btn-eliminar';
+
       btnEliminar.classList.add('btn', 'btn-danger', 'mr-2');
       btnEliminar.innerHTML =
         'Eliminar <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>';
