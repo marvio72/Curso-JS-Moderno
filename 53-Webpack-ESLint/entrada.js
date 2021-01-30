4391df61a2b49fc7289b55e8b84d3026872f5ddb
marvio72 <mruvalcaba@oversistemas.com>
2021-01-30 01:03:10 -0600

    Transpilando el codigo con Babel

---
+++ 53-Webpack-ESLint/entrada.js
@@ -0,0 +1,10 @@
+class Cliente {
+  constructor(nombre, apellido) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+  }
+}
+
+const hola = () => {
+  console.log('hola mundo en babel');
+};
